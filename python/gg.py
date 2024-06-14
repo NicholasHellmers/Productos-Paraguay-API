0df4428be6bc45ea599adafbdcb69c2c30d3d24c
Nicholas Hellmers <nihe1661@colorado.edu>
2024-06-13 20:11:51 -0600

    Cleaned up some of the code

--- python/gg.py
+++ python/gg.py
@@ -1,34 +1,55 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
 import pandas as pd
 
+from dataclasses import dataclass
+
 import time
 
+@dataclass(kw_only=True, slots=True)
 class Product:
-    def __init__(self, name, price, category, product_url, image_url):
-        self.name = name
-        self.price = price
-        self.category = category
-        self.product_url = product_url
-        self.image_url = image_url
+    name: str
+    price: str
+    category: str
+    product_url: str
+    image_url: str
+
+def construct_product(product: webdriver.remote.webelement.WebElement) -> (Product | None):
+    product_media = product.find_element(By.CLASS_NAME, "product-media")
+    product_body = product.find_element(By.CLASS_NAME, "product-body")
+
+    new_product = Product(
+        name = product_body.find_element(By.CLASS_NAME, "product-title").text.strip(),
+        price = product_body.find_element(By.CLASS_NAME, "new-price").text.strip(),
+        category = product_body.find_element(By.CLASS_NAME, "product-cat").text.strip(),
+        product_url = product_media.find_element(By.TAG_NAME, "a").get_attribute("href"),
+        image_url = product_media.find_element(By.TAG_NAME, "img").get_attribute("src")
+    )
+
+    return new_product if new_product else None
 
 def main():
     service = Service(executable_path="./driver/chromedriver")
     chrome_options = Options()
-    # chrome_options.add_argument("--headless")
-    # chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
+    print("Headless Chrome Running")
+
     start_time = time.time()
 
+    print("Consiguiendo productos de Gonzalez Gimenez")
+
     driver.get("https://www.gonzalezgimenez.com.py/buscador?q=&buscar=")
+
     exit_condition = False
+    
     while exit_condition == False:
         # Find elements with class name of "infinite-status-prompt"
         # and print the attributes present in the elements
@@ -47,36 +68,21 @@ def main():
 
                 # Now we can scan for products
                 products = driver.find_elements(By.CLASS_NAME, "col-md-4.col-lg-4.col-xl-3.col-12")
-
+                
                 print("Cantidad de productos: ", len(products))
-
-                def construct_product(product):
-                    product_media = product.find_element(By.CLASS_NAME, "product-media")
-                    product_body = product.find_element(By.CLASS_NAME, "product-body")
-
-                    new_product = Product(
-                        product_body.find_element(By.CLASS_NAME, "product-title").text.strip(),
-                        product_body.find_element(By.CLASS_NAME, "new-price").text.strip(),
-                        product_body.find_element(By.CLASS_NAME, "product-cat").text.strip(),
-                        product_media.find_element(By.TAG_NAME, "a").get_attribute("href"),
-                        product_media.find_element(By.TAG_NAME, "img").get_attribute("src")
-                    )
-
-                    return new_product
 
                 list_of_products = [ construct_product(product=product) for product in products ]
 
-                # write the products into a csv file called "gg_products.csv" in the output folder, if the file does not exist, it will be created
-                df = pd.DataFrame([vars(product) for product in list_of_products])
-                df.to_csv("./output/gg_products.csv", index=False)
+                for product in list_of_products:
+                    print(product)
 
-                # print(new_product.name, "\n", new_product.price, "\n", new_product.category, "\n", new_product.product_url, "\n", new_product.image_url, "\n\n")
+                # write the products into a csv file called "gg_products.csv" in the output folder, if the file does not exist, it will be created
+                df = pd.DataFrame(list_of_products)
+                df.to_csv("./output/gg_products.csv", index=False)
 
                 exit_condition = True
                 break
 
-        
-        
         # Scroll to the bottom of the page
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
@@ -84,7 +90,5 @@ def main():
 
     print("Execution time: ", time.time() - start_time)
 
-    
-    
 if __name__ == "__main__":
     main()
